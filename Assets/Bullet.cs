59984115932abb716bb5df62b5da6dca61285f78
林玉伟 <18648470106@gmail.com>
2018-01-31 21:27:44 +0800

    测试
    Signed-off-by: 林玉伟 <18648470106@gmail.com>

---
+++ Assets/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int _Speed;
+
+    //public int _Attack;
+    public GameObject _target;
+
+    private void Start()
+    {
+        // _Attack = 1;
+        _Speed = 2;
+        //Vector3.Lerp(transform.position, _target, Time.deltaTime * _Speed);
+        // Invoke("DestroySlef", 3);
+    }
+
+    private void Update()
+    {
+        if (_target == null) return;
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+        Debug.Log("Bullet Attack Enemy");
+        ChangeVision(other.gameObject);
+        DestroySlef();
+    }
+
+    private void DestroySlef()
+    {
+        ChangeVision(gameObject);
+        Destroy(gameObject);
+    }
+
+    private void Move()
+    {
+        var dir = _target.transform.position - transform.position;
+        transform.position += dir.normalized * Time.deltaTime * _Speed;
+    }
+
+    private static void ChangeVision(GameObject targetGameObject)
+    {
+        targetGameObject.transform.localScale = Vector3.one * 2;
+        Debug.Log("放大了！！！");
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private float _attackIntever; //攻击间隔
+    [SerializeField] private int _attackRadius;
+    private WaitForSeconds _attackWaitForSeconds;
+    [SerializeField] private GameObject _bulletGameObject;
+    [SerializeField] private GameObject _bulletTarget;
+    private Collider2D _collider2D;
+    private List<GameObject> _enemies;
+    private GameObject _target;
+
+    private void Start()
+    {
+        _enemies = new List<GameObject>();
+        _attackIntever = 0.5f;
+        _attackWaitForSeconds = new WaitForSeconds(_attackIntever);
+        StartCoroutine(AttackEnemies());
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+        Debug.Log("Attack");
+        _bulletTarget = other.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+            _enemies.Remove(other.gameObject);
+    }
+
+
+    private IEnumerator AttackEnemies()
+    {
+        var timer = 0;
+        while (true)
+        {
+            timer += 1;
+            Debug.Log(timer);
+            if (timer <= 10) continue;
+            if (_bulletTarget != null)
+            {
+                Instantiate(_bulletGameObject, transform.position, Quaternion.identity);
+                _bulletGameObject.GetComponent<Bullet>()._target = _bulletTarget;
+            }
+
+
+            yield return _attackWaitForSeconds;
+        }
+    }
+
+    private void AttackEnemies(GameObject targetEnemey)
+    {
+        Instantiate(_bulletGameObject, transform.position, Quaternion.identity);
+        _bulletGameObject.GetComponent<Bullet>()._target = _bulletTarget;
+    }
+}
